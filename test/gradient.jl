0ce1a9ea241cc058238e004b7adebe751dd7de63
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-10-15 19:02:05 +0200

    Test with jax.jit

--- test/gradient.jl
+++ test/gradient.jl
@@ -4,15 +4,26 @@ using PythonCall
 using Test
 
 jnp = pyimport("jax.numpy")
+jax = pyimport("jax")
 
 backend = AutoJAX()
 
 f(x) = jnp.sum(jnp.square(x))
+f_jit = jax.jit(f)
 
 x = float.(1:3)
 grad = similar(x)
 
-@test gradient(f, backend, x) == [2, 4, 6]
-@test gradient!(f, grad, backend, x) == [2, 4, 6]
-@test value_and_gradient(f, backend, x) == (14, [2, 4, 6])
-@test value_and_gradient!(f, grad, backend, x) == (14, [2, 4, 6])
+@testset "Without JIT" begin
+    @test gradient(f, backend, x) == [2, 4, 6]
+    @test gradient!(f, grad, backend, x) == [2, 4, 6]
+    @test value_and_gradient(f, backend, x) == (14, [2, 4, 6])
+    @test value_and_gradient!(f, grad, backend, x) == (14, [2, 4, 6])
+end
+
+@testset "With JIT" begin
+    @test gradient(f_jit, backend, x) == [2, 4, 6]
+    @test gradient!(f_jit, grad, backend, x) == [2, 4, 6]
+    @test value_and_gradient(f_jit, backend, x) == (14, [2, 4, 6])
+    @test value_and_gradient!(f_jit, grad, backend, x) == (14, [2, 4, 6])
+end
